67a0d81dd58632f6e65713dac6278725dfdf3431
cesar <quintacs@gmail.com>
2022-12-09 22:50:30 -0300

    Criação do layout da tela de agendamento

--- src/app/agenda/modelo/agenda-modelo.ts
+++ src/app/agenda/modelo/agenda-modelo.ts
@@ -2,8 +2,13 @@ export interface AgendaModel{
 
     id:number;
     dataAgendamento:Date;
+    dataAgendamentoFormatada:string;
     idCliente:number;
     idServicos:number[];
+    nomeCliente:string;
+    telefoneCliente:string;
+    celularCliente:string;
+    emailCliente:string;
 
 }
 
